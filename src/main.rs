e9a580fdbafe8dd2fbbff614442d8805eb49fe2e
vijay18vp <voidprotocol18@gmail.com>
2026-02-09 19:29:18 +0530

    Learned about The Numbers , Strings and how to match the types

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 fn main(){
-    let x: i8 = -12 ;
-    let y : u8 = 200 ;
-    let z : f32 = 12.345;
 
-    println!("x : {} , y : {} , z : {}" , x , y, z);
+    // Numbers 
+    let x: i8 = -12 ;
+    println!("x : {} " , x);
+    println!("{}" , std::mem::size_of_val(&x));
+   
+   // Initialise the String in RUST 
+   let name = String::from("Vijay Hiremath");
+   println!("{}" , name );
+
+   let char1 = name.chars().nth(78);
+   let char2 = name.chars().nth(4);
+   // Printing the character at some index 
+    match char1 {
+        Some(name) => println!("{}" , name),
+        None => println!("Nothing is there in that index ! "),
+    }
+    match char2 {
+        Some(name) => println!("{}" , name),
+        None => print!("Nothing is there in that index !")
+    }
+
 }
+
+// fn main(){
+
+//     let male_info = false ;
+//     let female_info = true;
+//     let dont_know_info = true ; 
+
+//     if male_info {
+//         print!("Male");
+//     }
+//     else if female_info {
+//         print!("Female");
+//     } 
+//     else if dont_know_info {
+//         print!("Dont know ! ");
+//     }
+// }
+
